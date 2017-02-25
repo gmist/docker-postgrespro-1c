e17be236960ba34470bf7e09e3555280355b4685
Serg Baburin <serg.baburin@gmail.com>
2017-02-26 00:03:03 +0300

    Use short (single-dash) docker parameters

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 docker run --name postgrespro-1c \
   -p 5432:5432 \
-  --detach \
-  --volume postgrespro-1c-data:/data \
-  --volume /etc/localtime:/etc/localtime:ro \
-  --env POSTGRES_PASSWORD=password \
+  -d \
+  -v postgrespro-1c-data:/data \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e POSTGRES_PASSWORD=password \
   alexanderfefelov/postgrespro-1c
