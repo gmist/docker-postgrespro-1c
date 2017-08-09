2089d299495efe563a7f29bd918cccbe8c6519bc
Serg Baburin <serg.baburin@gmail.com>
2017-08-10 02:33:30 +0300

    Tune for 4Gb RAM

--- container/postgresql.conf.sh
+++ container/postgresql.conf.sh
@@ -8,11 +8,47 @@ cat << EOF > $PGDATA/postgresql.conf
 
 listen_addresses = '*'
 
+
 #------------------------------------------------------------------------------
 # ERROR REPORTING AND LOGGING
 #------------------------------------------------------------------------------
 
 logging_collector = on
 log_line_prefix = '%m '
+
+
+#------------------------------------------------------------------------------
+# TUNE
+#------------------------------------------------------------------------------
+
+max_connections = 100
+shared_buffers = 1GB
+effective_cache_size = 3GB
+work_mem = 26214kB
+maintenance_work_mem = 512MB
+min_wal_size = 4GB
+max_wal_size = 8GB
+checkpoint_completion_target = 0.9
+wal_buffers = 16MB
+default_statistics_target = 500
+
+
+#------------------------------------------------------------------------------
+# 1C specifics stuff
+#------------------------------------------------------------------------------
+max_locks_per_transaction = 256
+enable_nestloop = off
+temp_buffers = 256MB
+random_page_cost = 1.3
+effective_io_concurrency = 5
+standard_conforming_strings = off
+escape_string_warning = off
+synchronous_commit = off
+fsync = on
+row_security = off
+bgwriter_delay = 20ms
+autovacuum = on
+autovacuum_max_workers = 4
+autovacuum_naptime = 20s
 
 EOF
